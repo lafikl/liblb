2ef09878bbeb47ebc1810234629d07de65340539
Khalid Lafi <lafi.khalid@gmail.com>
2017-06-02 09:54:02 +0300

    bounded: test key load spread

--- bounded/consistent_bounded_test.go
+++ bounded/consistent_bounded_test.go
@@ -9,7 +9,8 @@ func TestNewConsistentBounded(t *testing.T) {
 	lb := New("127.0.0.1", "192.0.0.1", "88.0.0.1", "10.0.0.1")
 
 	for i := 0; i < 1000; i++ {
-		_, err := lb.Balance(fmt.Sprintf("hello world %d", i))
+		// _, err := lb.Balance(fmt.Sprintf("hello world %d", i))
+		_, err := lb.Balance("hello world")
 		if err != nil {
 			t.Fatal(err)
 		}
